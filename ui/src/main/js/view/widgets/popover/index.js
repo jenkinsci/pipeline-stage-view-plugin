6246fbe92b1e39a4d814b30c305741a2fc9fd428
Sam Van Oort <samvanoort@gmail.com>
2016-05-10 10:13:18 -0400

    Remove unused lpopover placement option

--- ui/src/main/js/view/widgets/popover/index.js
+++ ui/src/main/js/view/widgets/popover/index.js
@@ -318,17 +318,6 @@ Popover.prototype.applyPlacement = function() {
             'top': onElementOffset.top,
             'left': onElementOffset.left + thisPopover.onElement.width() + 5
         });
-    } else if (placement === 'centered-above-element') {  // centered in window element and just above
-        var winWidth = $(theWindow).width();
-        var popoverWidth = thisPopover.popover.width();
-
-        var leftPlacement = ((winWidth - popoverWidth) / 2);
-        var topPlacement = thisPopover.onElement.offset().top - 50;
-
-        thisPopover.popover.css({
-            'top': topPlacement,
-            'left': leftPlacement
-        });
     }  else if (placement === 'left') {
         var onElementOffset = thisPopover.onElement.offset();
         thisPopover.popover.css({
