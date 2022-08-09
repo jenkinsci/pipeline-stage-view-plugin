489d3a2da5c4096336ee4557fcce95de29dd1dc3
Thorsten Scherler <scherler@gmail.com>
2022-08-09 18:27:12 +0200

    chore: [feature/handlebar_central] not working
    Signed-off-by: Thorsten Scherler <scherler@gmail.com>

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -37,7 +37,7 @@ builder.bundle('src/main/js/stageview_adjunct.js')
     .minify()
     .inDir('target/generated-adjuncts/org/jenkinsci/pipeline');
 builder.bundle('src/main/js/stageview.js')
-    // .withExternalModuleMapping('handlebars', 'handlebars:handlebars3')
+    .withExternalModuleMapping('handlebars', 'handlebars:handlebars3')
     .withExternalModuleMapping('moment', 'momentjs:momentjs2')
     .asJenkinsModuleResource()
     .export();
