8a5aa8e975629053abd4a63457734349560988bd
Bruno Verachten <gounthar@gmail.com>
2025-12-01 12:36:56 +0100

    Migrate Commons Lang from 2 to 3 and StringEscapeUtils to Commons Text (#401)
    Co-authored-by: Bruno Verachten <gounthar@users.noreply.github.com>

--- rest-api/src/main/java/com/cloudbees/workflow/rest/external/ChangeSetExt.java
+++ rest-api/src/main/java/com/cloudbees/workflow/rest/external/ChangeSetExt.java
@@ -27,7 +27,7 @@ import com.cloudbees.workflow.util.ModelUtil;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import hudson.scm.ChangeLogSet;
 import hudson.scm.RepositoryBrowser;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 
 import java.io.IOException;
